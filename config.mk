10f21f74d25eb53b109d115cab34e2e37fecc4c9
noobMushroom <mushroom019.coder@gmail.com>
2023-01-13 17:57:35 +0530

    changed master key

--- config.mk
+++ config.mk
@@ -7,12 +7,12 @@ VERSION = 6.4
 PREFIX = /usr/local
 MANPREFIX = ${PREFIX}/share/man
 
-X11INC = /usr/X11R6/include
-X11LIB = /usr/X11R6/lib
+X11INC = /usr/include/X11R6
+X11LIB = /usr/lib/X11R6
 
 # Xinerama, comment if you don't want it
-XINERAMALIBS  = -lXinerama
-XINERAMAFLAGS = -DXINERAMA
+# XINERAMALIBS  = -lXinerama
+# XINERAMAFLAGS = -DXINERAMA
 
 # freetype
 FREETYPELIBS = -lfontconfig -lXft
