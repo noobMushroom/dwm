e7c5ef72cb5d26e34fcfb2fbbc24a07114d3b7c9
mushroom <mushroom019.coder@gmail.com>
2024-06-27 22:24:38 +0530

    changed the fonts

--- config.def.h
+++ config.def.h
@@ -9,7 +9,9 @@ static const int swallowfloating =
     0;                        /* 1 means swallow floating windows by default */
 static const int showbar = 1; /* 0 means no bar */
 static const int topbar = 1;  /* 0 means bottom bar */
-static const char *fonts[] = {"monospace:size=10"};
+static const char *fonts[] = {
+    "JetBrains Mono:size=11",
+    "JoyPixels:pixelsize=11:antialias=true:autohint=true"};
 static const char dmenufont[] = "monospace:size=10";
 static const char col_gray1[] = "#222222";
 static const char col_gray2[] = "#444444";
